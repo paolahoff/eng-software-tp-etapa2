9679846f82b9fac3e832351ac3c797e55623219b
VitorCamargs <vitor.cmoura@inf.ufrgs.br>
2022-03-27 11:45:20 -0300

    arrumei as classes e adicionei a funcao de login

--- classes.py
+++ classes.py
@@ -12,86 +12,119 @@ class Jogo:
         self.__titulo = titulo
         self.__requisitos = requisitos
 
+
 lista_de_contas = []
 
-#General Class Contada pra colocar uns hash no meio, esse é só o esqueleto)
+
+# General Class Contada pra colocar uns hash no meio, esse é só o esqueleto)
+def logar(login, senha):
+    for conta in lista_de_contas:
+        if [login, senha] == [conta.login, conta.senha]:
+            print('logado')
+            return conta
+    else:
+        print('Usuario ou senha incorretos')
+        return None
+
+
 class Conta:
-    def __init__(self, Id):
-        self.Id = Id
+    def __init__(self):
+        self.Id = None
         self.login = None
         self.senha = None
         pass
-    
+
     def __criar_conta(self, login, senha):
-        pass
+        lista_de_contas.append(self)
+        self.login = login
+        self.senha = senha
+        self.Id = len(lista_de_contas)  # podemos fazer um jeito menos tantan de fazer isso depois
+        return
 
     def cadastrar_conta(self):
+
         while True:
             login = input("Digite seu login")
-            senha = input("Digite sua senha")
-            #faz um hash pra deixar mais seguro
-            for conta in lista_de_contas:
-                if conta.conferir_login(login) is True:
-                    print("Nome de usuario já está em uso")
-                    continue
-                #salvar conta na lista de contas
-                break
-    
-    def conferir_login(self, login):
-        if login == self.login:
-            return True
-        else:
-            return False
+            if login in [conta.login for conta in lista_de_contas]:
+                print('Login ja usado')
+                continue
 
-    def conferir_credenciais(self, login, senha):
-        if login == self.login and senha == self.senha:
-            return self.Id
-        else:
-            return None
+            else:
+                senha = input("Digite sua senha")
+                # faz um hash pra deixar mais seguro
+                self.__criar_conta(login, senha)
+                return
 
-    
-#Conta Specializations
-class Conta_Desenvolvedor(Conta):
-    def __init__(self, jogos):
-        self.__jogos = jogos #Lista de jogos
 
-class Conta_Provedor(Conta):
-    def __init__(self, maquina):
+# Conta Specializations
+class ContaDesenvolvedor(Conta):
+    def __init__(self):
+        super().__init__()
+        self.__jogos = list  # Lista de jogos
+
+    def cadastrar_jogo(self, novo_jogo):
+        self.__jogos.append(novo_jogo)
+        pass
+
+
+class ContaProvedor(Conta):
+    def __init__(self, maquina, Id):
+        super().__init__()
         self.__maquina = maquina
+        self.jogos = None
 
-class Conta_Jogador(Conta):
-    def __init__(self, creditos):
+    def baixar_jogo(self, jogo):
+        self.jogos.append(jogo)
+        pass
+
+
+class ContaJogador(Conta):
+    def __init__(self, creditos, Id):
+        super().__init__()
         self.__creditos = creditos
 
-#General Class Aluguel
+    def alugar_jogo(self):
+        pass
+
+
+# General Class Aluguel
 class Aluguel:
     def __init__(self):
         pass
 
-#Aluguel Specializations
-class Aluguel_Jogos(Aluguel):
-    def __init__(self):
-        pass
 
-class Aluguel_Maquinas(Aluguel):
+# Aluguel Specializations
+class AluguelJogos(Aluguel):
     def __init__(self):
-        pass
+        super().__init__()
 
-#General Class Relatório
+
+class AluguelMaquinas(Aluguel):
+    def __init__(self):
+        super().__init__()
+
+
+# General Class Relatório
 class Relatorio:
     def __init__(self):
         pass
 
-#Relatório Specializations
-class Relatorio_Desenvolvedor(Relatorio):
-    def __init__(self):
-        pass
 
-class Relatorio_Provedor(Relatorio):
+# Relatório Specializations
+class RelatorioDesenvolvedor(Relatorio):
     def __init__(self):
-        pass
+        super().__init__()
+
+
+class RelatorioProvedor(Relatorio):
+    def __init__(self):
+        super().__init__()
 
 
 def fazer_login():
     login = input("Digite seu login")
     senha = input("Digite sua senha")
+
+
+if __name__ == '__main__':
+    pass
