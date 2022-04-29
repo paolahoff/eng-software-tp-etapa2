c18c6e1f552acb24af466eada666e6ba3e4b0813
rrehlert <ricardo.r.e.2000@gmail.com>
2022-04-28 23:06:30 -0300

    começada classes de jogo e de desenvolvedor, alterada a maneira com que as maquinas e jogos são salvos na lista para facilitar o salvamento depois

--- classes.py
+++ classes.py
@@ -1,5 +1,10 @@
+import time
+from typing_extensions import Self
+
 
 lista_de_contas = []
+lista_de_maquinas = []
+lista_de_jogos = []
 
 
 class Maquina:
@@ -13,6 +18,7 @@ class Maquina:
         self.em_uso = False
         self.__horas_em_uso = 0
         self.__ganhos = 0
+        self.jogos = list()
 
     def zerar_ganhos(self):
         self.__ganhos = 0
@@ -23,6 +29,7 @@ class Maquina:
         self.__especificacoes = especificacoes
         self.__porcentagem_uso = porcentagem_uso
         return
+
     def __gerar_hora_aluguel(self):
         hora_aluguel = self.__porcentagem_uso * self.precos[self.__especificacoes]
         return hora_aluguel
@@ -36,13 +43,71 @@ class Maquina:
         return self
 
 
+    def buscar_jogo(self,titulo):
+        for jogo in lista_de_jogos:
+            if jogo.titulo == titulo:
+                return jogo
+        return None
+        pass
+
+    
+    def baixar_jogo(self, titulo):
+        jogo = self.buscar_jogo(titulo)
+        if jogo == None:
+            return None
+        else:
+            jogo.registrar_maquina() #pra possibilitar pesquisa por máquina ou por jogo
+            self.jogos.append(jogo) # Não sei se fazemos assim ou colocando apenas o Titulo
+        pass
+
 class Jogo:
-    def __init__(self, titulo, requisitos, valor):
+    def __init__(self):
+        self.__titulo = None
+        self.__requisitos = None
+        self.__valor = None
+        self.__desenvolvedor = None
+        self.__jogadores = list()
+        self.maquinas = list()
+        self.tempo_jogado = 0
+        self.inicio = None
+
+    def cadastrar(self, titulo, requisitos, valor, desenvolvedor):
+        self.__titulo = titulo
+        self.__requisitos = requisitos
+        self.__valor = valor
+        self.__desenvolvedor = desenvolvedor
+    
+        lista_de_jogos.append(self)
+        print(f'jogo {self} cadastrada')
+        return Self
+    
+    def editar(self, titulo, requisitos, valor):
         self.__titulo = titulo
         self.__requisitos = requisitos
         self.__valor = valor
 
+    def registrar_maquina(self, maquina):
+        self.maquinas.append(maquina)
 
+    def iniciar(self):
+        self.inicio = time.time()
+        return
+
+    def encerrar(self):
+        fim = time.time()
+        segundos_jogados = (fim - self.inicio)
+        horas_jogadas = segundos_jogados/3600
+        self.tempo_jogado = horas_jogadas
+        return
+    
+    def calcular_ganhos(self):
+        ganhos = round(self.tempo_jogado) * self.__valor
+        self.tempo_jogado = 0
+        return ganhos
+    
+
+    def get_desenvolvedor(self): #Viajei achando que ia precisar disso, mas vou deixar aqui vai que
+        return self.__desenvolvedor
 
 
 
@@ -71,16 +136,39 @@ class ContaDesenvolvedor(Conta):
         super().__init__()
         self.__jogos = list()  # Lista de jogos
         self.tipo_da_conta = 'desenvolvedor'
+        self.ganhos = 0
 
-    def cadastrar_jogo(self, novo_jogo):
+    def cadastrar_jogo(self, titulo, requisitos, valor):
+        novo_jogo = Jogo()
+        novo_jogo.cadastrar(titulo, requisitos, valor, self.login)
         self.__jogos.append(novo_jogo)
         pass
 
-    def editar_jogo(self):
+    def editar_jogo(self, titulo, titulo_novo, requisitos, valor):
+        for jogo in self.__jogos:
+            if jogo.titulo ==  titulo:
+                jogo.editar(titulo_novo, requisitos, valor)
         pass
 
-    def remover_jogo(self):
+    def remover_jogo(self, titulo):
+        for jogo in self.__jogos:
+            if jogo.titulo ==  titulo:
+                self.__jogos.remove(jogo)
         pass
+
+    def calcular_ganhos(self):           #Acho que vai ser melhor fazer a busca desse jeito, por mais que seja 
+        if self.__jogos is None:         #mais longo, pois vai ser mais fácil de guardar no banco de dados vulgo TXT
+            print("Nenhum jogo cadastrado")
+            for titulo_jogo in self.__jogos:
+                for jogo in lista_de_jogos:
+                    if jogo.titulo == titulo_jogo:
+                        self.ganhos = jogo.calcular_ganhos()
+
+    def sacar_ganhos(self):
+        creditos = self.calcular_ganhos
+        print(f"Sacado ganhos: R$ {creditos}")
+        self.ganhos = 0
+        return creditos
 
     def relatorio(self):
         pass
@@ -137,8 +225,10 @@ class ContaProvedor(Conta):
     def alterar_aluguel(self):  # Verificaremos a necessidade
         pass
 
-    def baixar_jogo(self, jogo):
-        self.__maquinas.jogos.append(jogo)
+    def cadastrar_jogo(self, nome_maquina, titulo):
+        for maquina in lista_de_maquinas:
+            if nome_maquina == maquina.nome:
+                maquina.baixar_jogo(titulo)
         pass
 
 
@@ -148,7 +238,8 @@ class ContaJogador(Conta):
         self.__creditos = 0
         self.tipo_da_conta = 'jogador'
 
-    def alugar_jogo(self):
+    def alugar_maquina(self):
+
         pass
 
 
